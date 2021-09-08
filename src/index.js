f7ec5f045e92692a5a906d415be827e785b8e4d2
Inmer <inmerbalcarcel@gmail.com>
2021-09-07 19:12:16 -0600

    animals json and images

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+import Animals from "./pages/Animals";
+import Home from "./pages/Home";
+import reportWebVitals from "./reportWebVitals";
+import { Navbar, NavDropdown } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <main>
+        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Lil’ Beasts Animal Shelter</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto"></Nav>
+              <Nav>
+                <Nav.Link href="/animals">Animals</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Route path="/" exact component={Home} />
+        <Route path="/animals" exact component={Animals} />
+      </main>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
